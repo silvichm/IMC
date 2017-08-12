7ae029ca7a31c5dccbdb7aa5614684cc79b58051
silvichm <30295515+silvichm@users.noreply.github.com>
2017-08-12 18:46:14 -0500

    Repositorio IMC
    Se añadieron los archivos del código fuente para el programa que calcula el indice de masa corporal (IMC)

---
+++ 1_a_Sw_IMC/WindowsApplication2/Form1.vb
@@ -0,0 +1,81 @@
+﻿Public Class Form1
+
+    Private Sub Label1_Click(sender As Object, e As EventArgs) Handles Label1.Click
+
+    End Sub
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+
+    End Sub
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        Dim peso, altura As Double
+        Dim imc As Double
+        peso = Val(TextBox1.Text)
+        altura = Val(TextBox2.Text)
+        imc = peso / (altura * altura)
+        If IsNumeric(TextBox1.Text) And Val(TextBox1.Text) > 0 Then
+            peso = Val(TextBox1.Text)
+        Else : MsgBox("Error, ingrese un número válido")
+            TextBox1.Focus()
+            Exit Sub
+        End If
+        If IsNumeric(TextBox2.Text) And Val(TextBox2.Text) > 0 Then
+            altura = Val(TextBox2.Text)
+        Else : MsgBox("Error, ingrese un número válido")
+            TextBox1.Focus()
+            Exit Sub
+        End If
+        TextBox3.Text = Str(imc)
+        If IsNumeric(TextBox3.Text) And Val(TextBox3.Text) < 18.5 Then
+            MsgBox("Clasificación: Bajo peso")
+            TextBox1.Focus()
+            Exit Sub
+        End If
+        If IsNumeric(TextBox3.Text) And Val(TextBox3.Text) = 18.5 Then
+            MsgBox("Clasificación: Peso Promedio")
+            TextBox1.Focus()
+            Exit Sub
+        End If
+        If IsNumeric(TextBox3.Text) And Val(TextBox3.Text) < 25 Then
+            MsgBox("Clasificación: Peso Promedio")
+            TextBox1.Focus()
+            Exit Sub
+        End If
+        If IsNumeric(TextBox3.Text) And Val(TextBox3.Text) = 25 Then
+            MsgBox("Clasificación: Aumentado")
+            TextBox1.Focus()
+            Exit Sub
+        End If
+        If IsNumeric(TextBox3.Text) And Val(TextBox3.Text) < 30 Then
+            MsgBox("Clasificación: Aumentado")
+            TextBox1.Focus()
+            Exit Sub
+        End If
+        If IsNumeric(TextBox3.Text) And Val(TextBox3.Text) = 30 Then
+            MsgBox("Clasificación: Moderado")
+            TextBox1.Focus()
+            Exit Sub
+        End If
+        If IsNumeric(TextBox3.Text) And Val(TextBox3.Text) < 35 Then
+            MsgBox("Clasificación: Moderado")
+            TextBox1.Focus()
+            Exit Sub
+        End If
+        If IsNumeric(TextBox3.Text) And Val(TextBox3.Text) = 35 Then
+            MsgBox("Clasificación: Severo")
+            TextBox1.Focus()
+            Exit Sub
+        End If
+        If IsNumeric(TextBox3.Text) And Val(TextBox3.Text) < 40 Then
+            MsgBox("Clasificación: Severo")
+            TextBox1.Focus()
+            Exit Sub
+        End If
+        If IsNumeric(TextBox3.Text) And Val(TextBox3.Text) >= 40 Then
+            MsgBox("Clasificación: Muy Severo")
+            TextBox1.Focus()
+            Exit Sub
+        End If
+    End Sub
+End Class
